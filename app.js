d89d32b72524709a404a24625e38f6d181349bba
Manuel Šarić <manujelko@gmail.com>
2022-12-30 12:16:35 +0100

    Use arrow functions.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let gameIsRunning = false;
 
-const getPlayerChoice = function () {
+const getPlayerChoice = () => {
     const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
     if (
         selection !== ROCK &&
@@ -23,7 +23,7 @@ const getPlayerChoice = function () {
     return selection;
 };
 
-const getComputerChoice = function () {
+const getComputerChoice = () => {
     const randomValue = Math.random();
     if (randomValue < 0.34) {
         return ROCK;
@@ -34,7 +34,7 @@ const getComputerChoice = function () {
     }
 };
 
-const getWinner = function (cChoice, pChoice) {
+const getWinner = (cChoice, pChoice) => {
     if (cChoice === pChoice) {
         return RESULT_DRAW;
     } else if (
@@ -47,7 +47,7 @@ const getWinner = function (cChoice, pChoice) {
     }
 }
 
-startGameBtn.addEventListener('click', function () {
+startGameBtn.addEventListener('click', () => {
     if (gameIsRunning) {
         return;
     }
